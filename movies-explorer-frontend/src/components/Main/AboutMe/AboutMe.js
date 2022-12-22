6bac9e147694322e407105731e50dc51baa78a19
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-12-22 22:10:43 +0300

    feat: add portfolio links

--- movies-explorer-frontend/src/components/Main/AboutMe/AboutMe.js
+++ movies-explorer-frontend/src/components/Main/AboutMe/AboutMe.js
@@ -4,15 +4,14 @@ function AboutMe()
 {  
     return (
       <article className="aboutMe">
-        <h2 className="aboutMe__title">Студент</h2>
+        <h2 className="aboutMe__title">Выпускник</h2>
         <div className="aboutMe__line"></div>
         <div className="aboutMe__wrapper">
             <div className="aboutMe__info">
                 <h3 className="aboutMe__info-title">Владимир</h3>
                 <h4 className="aboutMe__info-subtitle">Фронтенд-разработчик, 40 лет</h4>
-                <p className="aboutMe__info-text">Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ. У&nbsp;меня есть жена 
-и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить. С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;. После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.</p>
-                <a className="aboutMe__info-profile" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">Github</a>
+                <p className="aboutMe__info-text">Родился в&nbsp;Германии. Учился и&nbsp;окончил технический университет на&nbsp;Дальнем Востоке. Обосновался в&nbsp;Москве. Люблю готовить, путешествовать с&nbsp;семьей (у&nbsp;меня жена и&nbsp;двое сыновей) и&nbsp;увлекаюсь любительским бегом. Проработал 18&nbsp;лет в&nbsp;продажах и&nbsp;закупках. Наконец решился поменять свою жизнь и&nbsp;заняться тем, что действительно нравится&nbsp;&mdash; веб-разработкой. Обучился по&nbsp;специальности &laquo;Веб-разработчик&raquo; на&nbsp;курсах Яндекс Практикум. Ищу работу в&nbsp;команде разработчиков в&nbsp;замечательной Компании.</p>
+                <a className="aboutMe__info-profile" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">Моя страничка на Github Pages</a>
             </div>
             <div className="aboutMe__info-photo"></div>
         </div>
--- movies-explorer-frontend/src/components/Main/Portfolio/Portfolio.js
+++ movies-explorer-frontend/src/components/Main/Portfolio/Portfolio.js
@@ -7,12 +7,12 @@ function Portfolio()
         <ul className="portfolio__grid">
             <li className="portfolio__title">Портфолио</li>
             <li className="portfolio__item">
-                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">Статичный сайт</a>
-                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">↗</a>
+                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/cool-colours/">Pet-проект с генерацией рандомных цветов</a>
+                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/cool-colours/">↗</a>
             </li>
             <li className="portfolio__item">
-                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">Адаптивный сайт</a>
-                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">↗</a>
+                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/russian-travel/">Одностраничный сайт о путешествиях по России</a>
+                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/russian-travel/">↗</a>
             </li>
             <li className="portfolio__item">
                 <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">Одностраничное приложение</a>
