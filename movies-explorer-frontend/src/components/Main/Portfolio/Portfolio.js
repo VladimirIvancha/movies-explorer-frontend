f94ff921070fd01c63225c9a894b7cb7c786eb8a
Vladimir Ivancha <Vladimir.Ivancha@mail.ru>
2022-12-25 13:30:10 +0300

    feat: refactor portfolio items

--- movies-explorer-frontend/src/components/Main/Portfolio/Portfolio.js
+++ movies-explorer-frontend/src/components/Main/Portfolio/Portfolio.js
@@ -1,23 +1,25 @@
 import React, { memo } from "react";
+import PortfolioItem from "../Portfolio-item/PortfolioItem";
+import { initialPortfolioData } from "../../../utils/initial-portfolio-data";
 
 function Portfolio()
 {  
     return (
       <article className="portfolio">
-        <ul className="portfolio__grid">
+        <ul className="portfolio__list">
             <li className="portfolio__title">Портфолио</li>
-            <li className="portfolio__item">
-                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/cool-colours/">Pet-проект с генерацией рандомных цветов</a>
-                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/cool-colours/">↗</a>
-            </li>
-            <li className="portfolio__item">
-                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/russian-travel/">Одностраничный сайт о путешествиях по России</a>
-                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://vladimirivancha.github.io/russian-travel/">↗</a>
-            </li>
-            <li className="portfolio__item">
-                <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">Одностраничное приложение</a>
-                <a className="portfolio__icon" target="_blank" rel="noreferrer" href="https://github.com/VladimirIvancha">↗</a>
-            </li>
+            {
+                initialPortfolioData.map(item => 
+                   (
+                    <PortfolioItem 
+                    name={item.name}
+                    link={item.link}
+                    description={item.description}
+                    functional={item.functional}
+                    />
+                   ) 
+                )
+            }
         </ul>
       </article>
     );
